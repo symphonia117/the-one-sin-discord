9348d5545e8dd3f7a76ea8e36bef59a14470ec57
Jake Siegel <jake.seigel@gmail.com>
2018-03-23 05:45:12 +0300

    Implemented a "say" command

--- sin.js
+++ sin.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
@@ -9,6 +10,24 @@ client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
   }
+
+  else{
+
+	  	if(message.author.bot) return;
+
+	  	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	 	const command = args.shift().toLowerCase();
+
+	 	if(command == "say"){
+
+	 			const sayMessage = args.join(" ");
+
+	 			message.delete().catch(O_o =>{});
+
+	 			message.channel.send(sayMessage);
+
+	  	}
+ 	}
 });
 
 client.login(process.env.ACCESS_KEY);
